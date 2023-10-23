964f20fc5c9fbca45860be961b3356c398cbc746
Pedro Dias <PedroRibeiroDias@outlook.com>
2023-10-23 17:36:17 +0100

    Add debug config for vscode

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,23 +6,28 @@
 /*   By: pedribei <pedribei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 10:00:20 by pedribei          #+#    #+#             */
-/*   Updated: 2023/10/18 11:39:20 by pedribei         ###   ########.fr       */
+/*   Updated: 2023/10/23 16:53:53 by pedribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./get_next_line/get_next_line.h"
+#include "./project/get_next_line.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
 
-	fd = open("text.txt", O_RDONLY);
+	if (argc != 2)
+	{
+		printf("Missing file\n");
+		return (0);
+	}
+	fd = open(argv[1], O_RDONLY);
 	while ((line = get_next_line(fd)))
 	{
 		printf("%s\n", line);
