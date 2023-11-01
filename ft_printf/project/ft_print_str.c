407fc1e96540d2a723190a764a66bb0575cc080e
Pedro Dias <PedroRibeiroDias@outlook.com>
2023-11-01 20:58:45 +0000

    Add str and char functions

---
+++ ft_printf/project/ft_print_str.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_str.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedribei <pedribei@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 17:25:07 by pedribei          #+#    #+#             */
+/*   Updated: 2023/11/01 20:14:20 by pedribei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_str(char *str)
+{
+	if (!str)
+		return (write(1, "(null)", 6));
+	return (write(1, str, ft_strlen(str)));
+}
+
+int	ft_print_char(unsigned char c)
+{
+	return (write(1, &c, 1));
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	if (!str)
+		return (0);
+	len = 0;
+	while (*str++)
+		len++;
+	return (len);
+}
