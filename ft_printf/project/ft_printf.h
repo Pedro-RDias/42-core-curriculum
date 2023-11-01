1595d0c131a7909e9d277f22fb0666dc0717e010
Pedro Dias <PedroRibeiroDias@outlook.com>
2023-11-01 20:58:52 +0000

    Add header

--- ft_printf/project/ft_printf.h
+++ ft_printf/project/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pedribei <pedribei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:10:12 by pedribei          #+#    #+#             */
-/*   Updated: 2023/11/01 16:10:13 by pedribei         ###   ########.fr       */
+/*   Updated: 2023/11/01 20:08:57 by pedribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
+# include <unistd.h>
 
-int	ft_printf(const char *, ...);
+int		ft_printf(const char *format, ...);
+
+size_t	ft_strlen(const char *str);
+
+int		ft_print_str(char *str);
+int		ft_print_char(unsigned char c);
+int		ft_print_number(int n);
+int		ft_print_unsigned_number(unsigned long long n);
+int		ft_print_hex(unsigned int n, int uppercase);
+int		ft_print_base(unsigned long long n, char *base);
 
 #endif
